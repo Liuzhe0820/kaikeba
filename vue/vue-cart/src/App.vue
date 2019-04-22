512a0e445301a45b498ae36fd638d519b0557da6
liuzhe0820 <liu55zhe#163.com>
2019-04-22 16:32:41 +0800

    1

--- vue/vue-cart/src/App.vue
+++ vue/vue-cart/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <div>
+      <ul>
+        <li v-for='(goods,index) in goodsList' :key='goods.id'>
+          <span>{{goods.text}}</span>
+          <span>￥{{goods.pic}}</span>
+          <button @click=addGood(index)>加入购物车</button>
+        </li>
+      </ul>
+    </div>
+    <div>
       <cart :name='name' :cart='cart'></cart>
     </div>
   </div>
@@ -14,6 +23,41 @@ export default {
   name: 'app',
   components: {
     Cart
+  },
+  data(){
+    return {
+      name:'开课吧购物车',
+      cart:[],
+      goodsList:[
+        {
+          id:1,
+          text:'商品1',
+          pic:100
+        },
+        {
+          id:2,
+          text:'商品2',
+          pic:600
+        }
+      ]
+    }
+  },
+  methods:{
+    addGood(i){
+      const goods = this.goodsList[i];
+      const ret = this.cart.find((value)=>{
+        return value.id===goods.id
+      });
+      if(ret){
+        ret.count+=1
+      }else{
+        this.cart.push({
+          ...goods,
+          count:1,
+          acticve:true
+        })
+      }
+    }
   }
 }
 </script>
--- vue/vue-cart/src/components/Cart.vue
+++ vue/vue-cart/src/components/Cart.vue
@@ -1,14 +1,53 @@
 
 <template>
-    <div>购物车</div>
+<div>
+    <h3>{{name}}</h3>
+    <table border='1'>
+        <tr>
+            <th>课程名</th>
+            <th>单价</th>
+            <th>数量</th>
+            <th>价格</th>
+        </tr>
+        <tr v-for='(c,i) in cart' :key='c.id'>
+            <td>{{c.text}}</td>
+            <td>￥{{c.pic}}</td>
+            <td>
+                <button @click='minus(i)'>-</button>
+                {{c.count}}
+                 <button @click='add(i)'>+</button>
+                </td>
+            <td>￥{{c.pic*c.count}}</td>
+        </tr>
+    </table>
+</div>
+    
 </template>
 
 <script>
 export default {
-    name:'Cart'
-}
+  props: ["name", "cart"],
+  methods:{
+      minus(i){
+          const count = this.cart[i].count;
+          if(count>1){
+            this.cart[i].count-=1;
+          }else{
+              this.remove(i);
+          }
+          
+      },
+      add(i){
+          this.cart[i].count+=1;
+      },
+      remove(i){
+          if(window.confirm('确定删除？')){
+              this.cart.splice(i,1);
+          }
+      }
+  }
+};
 </script>
 
 <style>
-
 </style>
