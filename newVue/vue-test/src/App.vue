7e9cbeb2a2843894ff6a6cd809b24815199f19ea
e <liu55zhe@163.com>
2019-05-14 18:16:49 +0800

    1

--- newVue/vue-test/src/App.vue
+++ newVue/vue-test/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-test></form-test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import formTest from './components/formTest.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    formTest
   }
 }
 </script>
---
+++ newVue/vue-test/src/components/myFormItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <label v-if='label'>{{label}}</label>
+        <!-- 插槽 -->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
