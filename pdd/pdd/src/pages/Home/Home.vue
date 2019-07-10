c0bb83993894a73efd32cd240a680891db180ef9
e <liu55zhe@163.com>
2019-07-10 17:38:02 +0800

    1

--- pdd/pdd/src/pages/Home/Home.vue
+++ pdd/pdd/src/pages/Home/Home.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
-    首页
+    <ly-tab
+      v-model = 'selectedId'
+      :items='items'
+      :options='options'
+    >
+
+    </ly-tab>
   </div>
 </template>
 
 <script>
-  import Vue from 'vue'
+import Vue from "vue";
 
-  export default Vue.extend({
-
-  })
+export default Vue.extend({
+  name: "Home",
+  data() {
+    return {
+      selectedId: 0,
+      items: [
+        { label: "热门" },
+        { label: "服饰" },
+        { label: "鞋包" },
+        { label: "母婴" },
+        { label: "百货" },
+        { label: "食品" },
+        { label: "内衣" },
+        { label: "男装" },
+        { label: "电器" }
+      ],
+      options:{
+        activeColor: '#e9232c'
+      }
+    };
+  }
+});
 </script>
 
 <style  lang='stylus' ref='stylesheet/stylus'>
-
 </style>
