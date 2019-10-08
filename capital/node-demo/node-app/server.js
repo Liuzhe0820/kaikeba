9f27e59c535753c00dee096754d36d54c88e2476
e <liu55zhe@163.com>
2019-10-08 13:17:51 +0800

    1

--- capital/node-demo/node-app/server.js
+++ capital/node-demo/node-app/server.js
@@ -1,10 +1,18 @@
 const express = require('express');//引入
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();//实例化
+
 //引入注册登录接口
 const users = require('./routes/api/user');
 //引入 数据库地址
 const db = require('./config/keys').mongoURI;
+
+//使用body-parser中间件
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 mongoose.connect(db).then(()=>{
     console.log('mongonDB')
 }).catch((error)=>{
@@ -12,7 +20,10 @@ mongoose.connect(db).then(()=>{
 })
 app.get('/',(req,res)=>{
     res.send('hello,world!')
-})
+});
+//使用router
+app.use('/api/users',users);
+
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
